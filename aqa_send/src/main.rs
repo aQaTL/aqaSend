445d8121f7ab81ea4ac031940462dcc70666890e
aQaTL <mmsoltys@outlook.com>
2022-01-21 00:35:44 +0100

    Hello world

--- aqa_send/src/main.rs
+++ aqa_send/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
+use std::convert::Infallible;
+use std::net::SocketAddr;
 
+#[tokio::main]
+async fn main() {
+	let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+
+	let make_service = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle)) });
+
+	let server = Server::bind(&addr).serve(make_service);
+
+	let server_exit: Result<_, _> = server.await;
+	let _ = server_exit.unwrap();
+}
+
+async fn handle(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
+	Ok(Response::new(Body::from("Hello, World")))
 }

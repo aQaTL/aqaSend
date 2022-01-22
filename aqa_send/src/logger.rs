f2837d82cf613bc7171900e81f1d5110542e2256
aQaTL <mmsoltys@outlook.com>
2022-01-22 12:31:29 +0100

    Logger

---
+++ aqa_send/src/logger.rs
@@ -0,0 +1,27 @@
+use log::{LevelFilter, Metadata, Record};
+
+pub struct Logger;
+
+static LOGGER: Logger = Logger;
+
+pub fn init() {
+	log::set_logger(&LOGGER).expect("Tried set global logger twice");
+	log::set_max_level(LevelFilter::Debug);
+}
+
+impl log::Log for Logger {
+	fn enabled(&self, _metadata: &Metadata) -> bool {
+		true
+	}
+
+	fn log(&self, record: &Record) {
+		println!(
+			"[{}][{}] {}",
+			record.level(),
+			record.metadata().target(),
+			record.args(),
+		);
+	}
+
+	fn flush(&self) {}
+}
--- aqa_send/src/main.rs
+++ aqa_send/src/main.rs
@@ -1,11 +1,17 @@
 use hyper::service::{make_service_fn, service_fn};
 use hyper::{Body, Request, Response, Server};
+use log::*;
 use std::convert::Infallible;
 use std::net::SocketAddr;
 
+mod logger;
+
 #[tokio::main]
 async fn main() {
+	logger::init();
+
 	let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
+	info!("Bind address: {}", addr);
 
 	let make_service = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle)) });
 

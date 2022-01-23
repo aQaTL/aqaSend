99d242d9979fbe9fa4ac49645e94365c61fb29fe
aQaTL <mmsoltys@outlook.com>
2022-01-23 11:21:37 +0100

    Stupid anonymous lifetimes, let's just write out the Service implementations

--- aqa_send/src/main.rs
+++ aqa_send/src/main.rs
@@ -1,8 +1,12 @@
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
+use hyper::service::{make_service_fn, service_fn, Service};
+use hyper::{Body, Request, Response, Server, StatusCode};
 use log::*;
 use std::convert::Infallible;
+use std::error::Error;
+use std::fmt::{Display, Formatter};
+use std::future::{ready, Ready};
 use std::net::SocketAddr;
+use std::task::{Context, Poll};
 
 mod logger;
 
@@ -13,12 +17,69 @@ async fn main() {
 	let addr = SocketAddr::from(([127, 0, 0, 1], 8000));
 	info!("Bind address: {}", addr);
 
-	let make_service = make_service_fn(|_conn| async { Ok::<_, Infallible>(service_fn(handle)) });
-
-	let server = Server::bind(&addr).serve(make_service);
+	let server = Server::bind(&addr).serve(MakeService);
 
 	let server_exit: Result<_, _> = server.await;
 	let _ = server_exit.unwrap();
+}
+
+struct MakeService;
+
+impl<T> Service<T> for MakeService {
+	type Response = AqaService;
+	type Error = AqaServiceError;
+	type Future = Ready<Result<Self::Response, Self::Error>>;
+
+	fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+		Poll::Ready(Ok(()))
+	}
+
+	fn call(&mut self, _req: T) -> Self::Future {
+		ready(Ok(AqaService))
+	}
+}
+
+#[derive(Debug)]
+struct AqaService;
+
+#[derive(Debug)]
+enum AqaServiceError {
+	Hyper(hyper::Error),
+}
+
+impl Display for AqaServiceError {
+	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+		write!(f, "AqaServiceError")
+	}
+}
+
+impl Error for AqaServiceError {}
+
+impl From<hyper::Error> for AqaServiceError {
+	fn from(err: hyper::Error) -> Self {
+		AqaServiceError::Hyper(err)
+	}
+}
+
+impl Service<Request<Body>> for AqaService {
+	type Response = Response<Body>;
+	type Error = AqaServiceError;
+	type Future = std::future::Ready<Result<Self::Response, Self::Error>>;
+
+	fn poll_ready(&mut self, _cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+		Poll::Ready(Ok(()))
+	}
+
+	fn call(&mut self, req: Request<Body>) -> Self::Future {
+		println!("{:?}", req);
+		let body = Body::from(String::from("Hello, world"));
+		let resp = Response::builder()
+			.status(StatusCode::OK)
+			.body(body)
+			.unwrap();
+
+		ready(Ok(resp))
+	}
 }
 
 async fn handle(_req: Request<Body>) -> Result<Response<Body>, Infallible> {
---
+++ notes.md
@@ -0,0 +1,6 @@
+# Flushing out the structure
+
+## Start - the basics
+
+- [ ] Upload public files that will automatically be removed after one download
+	- That means we should have a directory called "1" in the "per_download_count" bucket

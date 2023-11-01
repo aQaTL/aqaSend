fe4fcae189e364816d885a8800eafb695614bf85
aQaTL <mmsoltys@outlook.com>
2023-11-01 23:38:54 +0100

    Deleting entries UI for My Files page

--- simple-website/my_files.js
+++ simple-website/my_files.js
@@ -3,6 +3,7 @@
 import * as Types from "./models.mjs"
 import {API_SERVER} from "./api_endpoint.mjs";
 import PasswordInput from "./components/password_input_dialog.mjs";
+import InfoMsgBox from "./components/info_msg_box.mjs";
 import * as Api from "./api.mjs";
 
 async function loadUser() {
@@ -63,12 +64,22 @@ function displayFiles(files) {
 		filenameEl.appendChild(document.createTextNode(file.filename));
 		fileLinkEl.appendChild(filenameEl);
 
+		let deleteButtonEl = document.createElement("button");
+		deleteButtonEl.type = "button";
+		deleteButtonEl.addEventListener("click", (/**@type{Event}*/ev) => {
+				ev.preventDefault();
+				deleteFileEntry(ev, file);
+		});
+		deleteButtonEl.appendChild(document.createTextNode("🗑️"))
+		deleteButtonEl.className = "deleteButton";
+		fileLinkEl.appendChild(deleteButtonEl);
 
 		if (file.visibility === "Private") {
 			fileEntryEl.className += " fileEntryPrivate";
 		}
 
 		let statsEl = document.createElement("div");
+		statsEl.className = "fileEntryStats";
 		{
 			let div = document.createElement("div");
 
@@ -120,3 +131,40 @@ function formatDuration(ms) {
 		.join(", ");
 }
 
+/**
+ *
+ * @param {Event} event
+ * @param {Types.FileModel} file
+ * @returns {Promise<void>}
+ */
+async function deleteFileEntry(event, file) {
+	const infoBox = InfoMsgBox.getById("infoBox");
+	infoBox.displayFailure(`Deleting ${file.filename}`)
+	infoBox.scrollIntoView({behavior: "smooth", block: "end"});
+
+	const request = new XMLHttpRequest();
+	request.addEventListener("load", (_event) => {
+		// We're expecting 204 no content
+		if (request.status === 204) {
+			infoBox.displaySuccess("Deleted successfully");
+		} else {
+			/** @type {Types.ErrorJsonBody} */
+			const response = request.response;
+
+			console.error("error: " + response && JSON.stringify(response) || "");
+			infoBox.displayFailure(response.message);
+		}
+	});
+
+	request.addEventListener("error", (/**@type{ErrorEvent}*/_event) => {
+		/** @type {Types.ErrorJsonBody} */
+		const response = request.response;
+
+		console.error("error: " + (response && JSON.stringify(response) || ""));
+		infoBox.displayFailure(response?.message ?? _event.message ?? "Failed to delete");
+	});
+
+	request.responseType = "json";
+	request.open("DELETE", `${API_SERVER}/api/delete/${file.uuid}`);
+	request.send();
+}
